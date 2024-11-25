a8b9ea7c37a168048e2d130fe610eb7d30f7a515
Nissan <dev.nissan@jsencoder.com>
2024-11-25 12:38:34 +0600

    3-problem solved

--- day1.js
+++ day1.js
@@ -17,20 +17,42 @@ const replacerFunction = (
 
 // console.log(replacerFunction([2,4,2,4],2,1))
 
-
 // 2. Write a function that will returns the sum of two numbers. and write another function that will return the sum # of numbers
 
 const sumNumbers = (num1, num2) => {
-    return num1 + num2;
-  };
-  
-  // console.log(sumNumbers(5,5))
-  
-  const sumAllNumbers = (...numbers) => {
-    console.log(numbers);
-    return numbers?.reduce((sumAllNumbers, num) => {
-      return sumAllNumbers + num;
-    });
-  };
-  
-  console.log(sumAllNumbers(1, 2, 1, 6));
+  return num1 + num2;
+};
+
+// console.log(sumNumbers(5,5))
+
+const sumAllNumbers = (...numbers) => {
+  console.log(numbers);
+  return numbers?.reduce((sumAllNumbers, num) => {
+    return sumAllNumbers + num;
+  });
+};
+
+//   console.log(sumAllNumbers(1, 2, 1, 6));
+
+
+
+
+// 3 write a function that will add  picture's border
+
+const pictures = ["abc", "def", "hlel"];
+
+const addBorder = (pictures) => {
+  let border = "*".repeat(pictures[0]?.length + 2);
+
+  pictures?.unshift(border);
+  pictures?.push(border);
+
+  pictures?.forEach((picture, index) => {
+    if (index > 0 && index < pictures.length - 1) {
+      pictures[index] = "*".concat(picture, "*");
+    }
+  });
+  return pictures;
+};
+
+// console.log(addBorder(pictures))
